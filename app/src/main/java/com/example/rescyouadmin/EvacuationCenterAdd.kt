b609f1dffe68ccb52ff52669fe749dc289c31209
russelhanapi <russelaminkadraphanapi@iskolarngbayan.pup.edu.ph>
2023-12-24 00:39:34 +0800

    Clean code

--- app/src/main/java/com/example/rescyouadmin/EvacuationCenterAdd.kt
+++ app/src/main/java/com/example/rescyouadmin/EvacuationCenterAdd.kt
@@ -2,13 +2,13 @@ package com.example.rescyouadmin
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Toast
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.rescyouadmin.databinding.ActivityEvacuationCenterAddBinding
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.libraries.places.api.Places
@@ -16,13 +16,13 @@ import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.api.model.RectangularBounds
 import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.AutocompleteActivity
-import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 
 private const val TAG = "EvacuationCenterAdd"
+
 class EvacuationCenterAdd : AppCompatActivity() {
 
     private val AUTOCOMPLETE_REQUEST_CODE = 9001
@@ -30,91 +30,55 @@ class EvacuationCenterAdd : AppCompatActivity() {
     private lateinit var binding: ActivityEvacuationCenterAddBinding
     private lateinit var data: FirebaseDatabase
     private lateinit var databaseReference: DatabaseReference
-    private lateinit var eventListener: ValueEventListener
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var dataList: MutableList<DataClass>
-//  private lateinit var adapter: DisasterAdapter
-    private lateinit var autocompleteSupportFragment: AutocompleteSupportFragment
 
     // Variable Declarations
-    var name: String? = null
-    var address: String? = null
-    var latitude: String? = null
-    var longitude: String? = null
+    private var name: String? = null
+    private var address: String? = null
+    private var latitude: String? = null
+    private var longitude: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEvacuationCenterAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initializeUI()
+    }
 
+    private fun initializeUI() {
+        setupBackButton()
+        setupPlacesAutocomplete()
+        setupStatusDropdown()
 
-        // Back Button
-        binding.backButton.setOnClickListener {
-            val intent = Intent(this, EvacuationCenters::class.java)
-            startActivity(intent)
+        binding.saveEvacuationCenterButton.setOnClickListener {
+            saveEvacuationCenter()
         }
 
-        // Places Autocomplete
-        // Initialize Places API with your API key
+        binding.cancelEvacuationCenterButton.setOnClickListener {
+            navigateToEvacuationCenters()
+        }
+    }
+
+    private fun setupBackButton() {
+        binding.backButton.setOnClickListener {
+            navigateToEvacuationCenters()
+        }
+    }
+
+    private fun setupPlacesAutocomplete() {
         Places.initialize(applicationContext, getString(R.string.api_key))
-        binding.nameTextInput.setOnTouchListener { view, event ->
+        binding.nameTextInput.setOnTouchListener { _, event ->
             if (event.action == MotionEvent.ACTION_DOWN) {
-                // This block will be executed when the user touches down on the view
                 startAutocompleteActivity()
             }
-            true // Return true to consume the event
+            true
         }
-
-        // Status Dropdown
-        val status = resources.getStringArray(R.array.status_evacuation_center)
-        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, status)
-        binding.statusDropdown.setAdapter(arrayAdapter)
-
-        // Save Button
-        binding.saveEvacuationCenterButton.setOnClickListener {
-
-            val nameInput = binding.nameTextInput.text.toString().trim()
-            val inChargeInput = binding.inChargeTextInput.text.toString().trim()
-            val contactNumInput = binding.inChargeContactNumTextInput.text.toString().trim()
-            val occupantsInput = binding.occupantsTextInput.text.toString().trim()
-
-            // Input Validation
-            if (nameInput.isEmpty()) {
-                binding.nameTextInput.error = "Place is required."
-                binding.nameTextInput.requestFocus()
-            } else if (inChargeInput.isEmpty()) {
-                binding.inChargeTextInput.error = "In-Charge is required."
-                binding.inChargeTextInput.requestFocus()
-            } else if (contactNumInput.isEmpty()) {
-                binding.inChargeContactNumTextInput.error = "Contact Number is required."
-                binding.inChargeContactNumTextInput.requestFocus()
-            } else if (occupantsInput.isEmpty()) {
-                binding.occupantsTextInput.error = "Occupants is required."
-                binding.occupantsTextInput.requestFocus()
-            } else {
-                data = FirebaseDatabase.getInstance()
-                databaseReference = data.getReference("Evacuation Centers")
-                val evacuationCenterData = EvacuationCenterData(name, address, latitude, longitude, "Available", inChargeInput, contactNumInput, occupantsInput)
-                databaseReference.child(name!!).setValue(evacuationCenterData)
-                Toast.makeText(this, "Evacuation Center added successfully.", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, EvacuationCenters::class.java)
-                startActivity(intent)
-            }
-        }
-
-        // Cancel Button
-        binding.cancelEvacuationCenterButton.setOnClickListener {
-            val intent = Intent(this, EvacuationCenters::class.java)
-            startActivity(intent)
-        }
-
     }
 
     private fun startAutocompleteActivity() {
         val canlubangBounds = RectangularBounds.newInstance(
-            LatLng(14.2049, 121.0910), // Southwest corner of the bounding box
-            LatLng(14.2110, 121.0975)  // Northeast corner of the bounding box
+            LatLng(14.2049, 121.0910),
+            LatLng(14.2110, 121.0975)
         )
 
         val intent = Autocomplete.IntentBuilder(
@@ -126,31 +90,96 @@ class EvacuationCenterAdd : AppCompatActivity() {
         startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
     }
 
+    private fun setupStatusDropdown() {
+        val status = resources.getStringArray(R.array.status_evacuation_center)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, status)
+        binding.statusDropdown.setAdapter(arrayAdapter)
+    }
+
+    private fun saveEvacuationCenter() {
+        val nameInput = binding.nameTextInput.text.toString().trim()
+        val inChargeInput = binding.inChargeTextInput.text.toString().trim()
+        val contactNumInput = binding.inChargeContactNumTextInput.text.toString().trim()
+        val occupantsInput = binding.occupantsTextInput.text.toString().trim()
+
+        if (isValidInput(nameInput, inChargeInput, contactNumInput, occupantsInput)) {
+            data = FirebaseDatabase.getInstance()
+            databaseReference = data.getReference("Evacuation Centers")
+            val evacuationCenterData = EvacuationCenterData(
+                name, address, latitude, longitude, "Available",
+                inChargeInput, contactNumInput, occupantsInput
+            )
+            databaseReference.child(name!!).setValue(evacuationCenterData)
+            showToast("Evacuation Center added successfully.")
+            navigateToEvacuationCenters()
+        }
+    }
+
+    private fun isValidInput(
+        nameInput: String,
+        inChargeInput: String,
+        contactNumInput: String,
+        occupantsInput: String
+    ): Boolean {
+        return when {
+            nameInput.isEmpty() -> {
+                showErrorAndFocus(binding.nameTextInput, "Place is required.")
+                false
+            }
+            inChargeInput.isEmpty() -> {
+                showErrorAndFocus(binding.inChargeTextInput, "In-Charge is required.")
+                false
+            }
+            contactNumInput.isEmpty() -> {
+                showErrorAndFocus(binding.inChargeContactNumTextInput, "Contact Number is required.")
+                false
+            }
+            occupantsInput.isEmpty() -> {
+                showErrorAndFocus(binding.occupantsTextInput, "Occupants is required.")
+                false
+            }
+            else -> true
+        }
+    }
+
+    private fun showErrorAndFocus(view: EditText, errorMessage: String) {
+        view.error = errorMessage
+        view.requestFocus()
+    }
+
+    private fun navigateToEvacuationCenters() {
+        startActivity(Intent(this, EvacuationCenters::class.java))
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showSnackbar(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
             when (resultCode) {
                 Activity.RESULT_OK -> {
-                    // Get the user's selected place from the Intent.
                     val place = Autocomplete.getPlaceFromIntent(data!!)
-
                     name = place.name
                     address = place.address
                     longitude = place.latLng?.longitude.toString()
                     latitude = place.latLng?.latitude.toString()
-
                     binding.nameTextInput.setText(place.name)
-
                     Log.i(TAG, "Place: $name, $address, $longitude, $latitude")
-
                 }
                 AutocompleteActivity.RESULT_ERROR -> {
                     val status = Autocomplete.getStatusFromIntent(data!!)
-                    // Handle the error status
+                    Snackbar.make(binding.root, status.statusMessage!!, Snackbar.LENGTH_SHORT).show()
+
                 }
                 Activity.RESULT_CANCELED -> {
-                    // The user canceled the operation
+                    Log.i(TAG, "The user cancelled the operation.")
                 }
             }
         }
