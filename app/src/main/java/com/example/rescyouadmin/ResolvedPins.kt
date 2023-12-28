05245076df65a17967a7c8e72119d4201f8dcde2
tnrtxx <tnrtxx@gmail.com>
2023-12-28 17:04:34 +0800

    Resolved Pins in progress

---
+++ app/src/main/java/com/example/rescyouadmin/ResolvedPins.kt
@@ -0,0 +1,11 @@
+package com.example.rescyouadmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResolvedPins : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resolved_pins)
+    }
+}
